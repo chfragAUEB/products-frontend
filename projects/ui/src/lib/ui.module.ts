1ade2c36102dabcecb2f2d14daa4f02bce219a83
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2023-04-24 19:27:46 +0300

    ui module

--- projects/ui/src/lib/ui.module.ts
+++ projects/ui/src/lib/ui.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
 import { UiComponent } from './ui.component';
@@ -6,7 +7,7 @@ import { DropdownComponent } from './dropdown/dropdown.component';
 
 @NgModule({
   declarations: [UiComponent, DropdownComponent],
-  imports: [RouterModule],
-  exports: [UiComponent],
+  imports: [BrowserModule, RouterModule],
+  exports: [UiComponent, DropdownComponent],
 })
 export class UiModule {}
